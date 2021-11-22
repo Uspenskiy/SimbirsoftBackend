b3b5e2e34c0bfc330b3512306519ca3ec93032a6
Lenovo <uspenskiy@outlook.com>
2021-11-23 04:13:25 +0500

    Опеделил все контроллеры

--- Api/Controllers/GenreController.cs
+++ Api/Controllers/GenreController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Dto;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,11 +8,48 @@ using System.Threading.Tasks;
 namespace Api.Controllers
 {
     /// <summary>
-    /// 2.7.3 - Контроллер авторы
+    /// 2.7.4. - Контроллер жанры:
     /// </summary>
     [ApiController]
     [Route("[controller]")]
     public class GenreController : ControllerBase
     {
+        public GenreController()
+        {
+
+        }
+
+        /// <summary>
+        /// 2.7.4.1. - Просмотр всех жанров. (без книг) 
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public async Task<IEnumerable<GenreDto>> GetGenres()
+        {
+            return new List<GenreDto>();
+        }
+
+        /// <summary>
+        /// 2.7.4.3.	Вывод статистики Жанр - количество книг
+        /// </summary>
+        /// <param name="dto"></param>
+        /// <returns></returns>
+        [HttpGet("Statistic/{id}")]
+        public async Task<int> GetStatistic(GenreDto dto)
+        {
+            return 0;
+        }
+
+        /// <summary>
+        /// 2.7.4.2. - Добавление нового жанра. (без книги)
+        /// </summary>
+        /// <param name="dto"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public async Task<GenreDto> AddGenre(GenreDto dto)
+        {
+            return dto;
+        }
+
     }
 }
